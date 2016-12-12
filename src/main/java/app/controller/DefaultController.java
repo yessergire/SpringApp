d7c5bc580a44008d7ee12b9741e9a1501408ab6e
Yessergire Mohamed <yessergire.mohamed@cs.helsinki.fi>
2016-12-13 00:08:15 +0200

    first commit

---
+++ src/main/java/app/controller/DefaultController.java
@@ -0,0 +1,12 @@
+package app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DefaultController {
+  @RequestMapping("*")
+  public String handleDefault() {
+    return "home";
+  }
+}
